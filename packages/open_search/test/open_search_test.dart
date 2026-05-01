b1b9db1dc78909f26be97033bc754d624159e89a
Andrii Ivanov <this.andrey@gmail.com>
2026-05-01 16:53:04 +0300

    init

---
+++ packages/open_search/test/open_search_test.dart
@@ -0,0 +1,16 @@
+import 'package:open_search/open_search.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
